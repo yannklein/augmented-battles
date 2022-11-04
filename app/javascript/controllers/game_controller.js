ce10678dac4d2dd93b1444927857bef95d4f623c
Yann Klein <yann.klein@me.com>
2022-11-04 13:31:41 +0900

    created basic stimulus conbtroller with mind-ar

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -1,9 +1,50 @@
-import { Controller } from "@hotwired/stimulus"
-import 'mind-ar/dist/mindar-image-three.prod.js';
+import { Controller } from "@hotwired/stimulus";
+import "mind-ar/dist/mindar-image-three.prod.js";
 
 // Connects to data-controller="game"
 export default class extends Controller {
+  static targets = ['start', 'stop', 'container']
+  
   connect() {
-    console.log("Hello from GameController")
+    console.log("Hello from GamesController")
+    this.imageTargetUrl = "/characters/targets.mind"
+    this.imageTargetCount = 16
+    this.imageTargetMax = 5
+    this.initMindAR()
+  }
+
+  async start(){
+    const { renderer, scene, camera } = this.mindarThree;
+    await this.mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  };
+
+  async stop() {
+    this.mindarThree.stop();
+    this.mindarThree.renderer.setAnimationLoop(null);
+  }
+
+  initMindAR() {
+    const THREE = window.MINDAR.IMAGE.THREE;
+    this.mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: this.containerTarget,
+      maxTrack: this.imageTargetMax,
+      imageTargetSrc: this.imageTargetUrl,
+    });
+    
+    for (let i=0; i < this.imageTargetCount; i+=1) {
+      const anchor = this.mindarThree.addAnchor(i);
+      const geometry = new THREE.PlaneGeometry(1, 0.55);
+      const material = new THREE.MeshBasicMaterial({
+        color: 0x00ffff,
+        transparent: true,
+        opacity: 0.5,
+      });
+      const plane = new THREE.Mesh(geometry, material);
+      anchor.group.add(plane);
+    }
+    this.start()
   }
 }
