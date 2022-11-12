676e78dc7ba8deb1d679d8f0d04ec1e13f6e5dca
Yann Klein <yann.klein@me.com>
2022-11-12 12:25:20 +0900

    created seeds for one game

--- app/models/soldier.rb
+++ app/models/soldier.rb
@@ -1,21 +1,9 @@
 class Soldier < ApplicationRecord
   belongs_to :army
   CATEGORIES = [
-    :alien,
-    :ancestor,
-    :black_hole,
-    :eva,
-    :gaia,
-    :god_eye,
-    :light_cone,
-    :lilith,
-    :nova,
-    :ocean,
-    :octopod,
-    :omni_eye,
-    :rising_sun,
-    :shell,
-    :triangle
+    :m1,
+    :m2,
+    :m3
   ]
   enum :category, CATEGORIES
   validates :name, :category, :skirmish_power, :distance_power, :max_distance, :speed, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,30 @@ User.destroy_all
 puts "Destroyed!"
 
 puts "Create users..."
-User.create!(email: "yann@me.com", password: "password")
-User.create!(email: "luc@me.com", password: "password")
+u1 = User.create!(email: "yann@me.com", password: "password")
+u2 = User.create!(email: "luc@me.com", password: "password")
+puts "Created"
+
+
+puts "Create games..."
+game = Game.create!(
+  archived: false, 
+  user: u1,
+  turn: u1,
+)
+puts "Created"
+
+puts "Create armies..."
+army1 = Army.create!(
+  category: 1, 
+  user: u1,
+  game: game,
+)
+army1.populate(3)
+army2 = Army.create!(
+  category: 2, 
+  user: u2,
+  game: game,
+)
+army1.populate(3)
 puts "Created"
